ba3337ffb106fe29130ccc214848b52adeb1dc56
Daniel Shteinbok <dshteinbok@gmail.com>
2021-06-16 11:44:07 -0400

    added pre-built graal-native spring enclave from spring initializer

---
+++ enclave/src/test/java/com/example/graaloidc/GraalOidcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.enclave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalOidcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
